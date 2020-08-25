4b2b146b7fef8f55558e71eca49eaae419496a79
Amanda Morris <amandahenry2@comcast.net>
2020-08-24 19:29:16 -0600

    added skeleton of components

---
+++ src/components/NavTabs.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavTabs() {
+  // We'll go into the Hooks API later, for now, we are just using some code
+  // from the react-router docs (https://reacttraining.com/react-router/web/api/Hooks/uselocation)
+  // This allows the component to check the route any time the user uses a link to navigate.
+  const location = useLocation();
+
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/portfolio"
+          className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+        >
+          Portfolio
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link
+          to="/contact"
+          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </Link>
+      </li>
+
+    </ul>
+  );
+}
+
+export default NavTabs;
