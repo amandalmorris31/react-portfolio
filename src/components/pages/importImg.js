f4915ec7c692a342eb5aa0158c238f2ded3ee2cc
Amanda Morris <amandahenry2@comcast.net>
2020-09-08 09:13:50 -0600

    moved images under public directory and added importImg file under components/pages

---
+++ src/components/pages/importImg.js
@@ -0,0 +1,11 @@
+const images =[
+    {
+      id:1,
+      src: "./images/CodeConnectScreenshot.jpg",
+      href:"http://project-02-code-connect.herokuapp.com/",
+      github:"https://github.com/amandalmorris31/project-two",
+      title:"Code Connect Hosted on Heroku"
+    }
+  ];
+
+  export default images;
